11c5f7b3475f8859341c1898a600e3c4b8be3567
Ajay <ajay@saturam.com>
2025-05-29 09:27:36 +0530

    added required instructions

--- dynamic_api.py
+++ dynamic_api.py
@@ -3,7 +3,8 @@ import psycopg2
 from psycopg2 import sql
 from typing import Dict, List
 from datetime import datetime,timedelta,timezone
-
+import logging
+import traceback
 # Create main app
 app = Flask(__name__)
 
@@ -80,7 +81,7 @@ def insert_endpoint_to_db(endpoint_name, endpoint_path, query_str, created_by, c
     conn = psycopg2.connect(**DB_CONFIG)
     cursor = conn.cursor()
     cursor.execute("""
-        INSERT INTO endpoint_registry (endpoint_name, endpoint_path, query, created_by, created_at)
+        INSERT INTO de_dynamic_api.endpoint_registry (endpoint_name, endpoint_path, query, created_by, created_at)
         VALUES (%s, %s, %s, %s, %s)
         RETURNING id;
     """, (endpoint_name, endpoint_path, query_str, created_by, created_at))
@@ -94,7 +95,7 @@ def endpoint_exists(endpoint_name: str) -> bool:
     conn = psycopg2.connect(**DB_CONFIG)
     cursor = conn.cursor()
     cursor.execute(
-        "SELECT 1 FROM endpoint_registry WHERE endpoint_name = %s LIMIT 1;",
+        "SELECT 1 FROM de_dynamic_api.endpoint_registry WHERE endpoint_name = %s LIMIT 1;",
         (endpoint_name,)
     )
     exists = cursor.fetchone() is not None
@@ -106,7 +107,7 @@ def get_query_by_endpoint_name(endpoint_name: str):
     conn = psycopg2.connect(**DB_CONFIG)
     cursor = conn.cursor()
     cursor.execute("""
-        SELECT id, query FROM endpoint_registry WHERE endpoint_name = %s
+        SELECT id, query FROM de_dynamic_api.endpoint_registry WHERE endpoint_name = %s
     """, (endpoint_name,))
     result = cursor.fetchone()
     cursor.close()
@@ -132,74 +133,85 @@ def log_endpoint_usage(endpoint_id: int, accessed_at: str):
 @app.route('/generate-endpoint', methods=['POST'])
 def generate_endpoint():
     """ Generates a dynamic endpoint and a dynamic query will be stored in the database."""
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    table_name = data.get('table_name')
-    schema_name = data.get('schema_name', 'public')
-    filter_columns = data.get('filter_columns', [])
-    select_columns = data.get('select_columns', [])
-    created_by = data.get('created_by', 'anonymous')
+        table_name = data.get('table_name')
+        schema_name = data.get('schema_name', 'public')
+        filter_columns = data.get('filter_columns', [])
+        select_columns = data.get('select_columns', [])
+        created_by = data.get('created_by', 'anonymous')
 
-    if not table_name or not select_columns:
-        return jsonify({'error': 'Both table_name and select_columns are required'}), 400
+        if not table_name or not select_columns:
+            return jsonify({'error': 'Both table_name and select_columns are required'}), 400
 
-    schema = get_table_schema(schema_name, table_name)
-    if not schema:
-        return jsonify({'error': f'Table {table_name} not found'}), 404
+        schema = get_table_schema(schema_name, table_name)
+        if not schema:
+            return jsonify({'error': f'Table {table_name} not found'}), 404
 
-    # Validate columns exist in table
-    for col in select_columns + filter_columns:
-        if col not in schema:
-            return jsonify({'error': f'Column {col} not found in table {table_name}'}), 400
+        # Validate columns exist in table
+        for col in select_columns + filter_columns:
+            if col not in schema:
+                return jsonify({'error': f'Column {col} not found in table {table_name}'}), 400
 
-    endpoint_name = generate_endpoint_name(table_name, select_columns, filter_columns)
-    endpoint_path = f'/dynamic/{endpoint_name}'
+        endpoint_name = generate_endpoint_name(table_name, select_columns, filter_columns)
+        endpoint_path = f'/dynamic/{endpoint_name}'
 
-    if endpoint_exists(endpoint_name):
-        return jsonify({'error': f'Endpoint already exists at {endpoint_path}'}), 400
+        if endpoint_exists(endpoint_name):
+            return jsonify({'Info': f'Endpoint already exists at {endpoint_path}'}), 400
 
+        
+        query_str = make_dynamic_query(schema_name, table_name, select_columns, filter_columns)
+        created_at = get_current_datetime()
+        insert_endpoint_to_db(endpoint_name, endpoint_path, query_str, created_by, created_at)
+
+        return jsonify({
+            'message': f'Endpoint created at {endpoint_path}',
+            'endpoint_path': endpoint_path,
+            'method': 'GET'
+        })
     
-    query_str = make_dynamic_query(schema_name, table_name, select_columns, filter_columns)
-    created_at = get_current_datetime()
-    insert_endpoint_to_db(endpoint_name, endpoint_path, query_str, created_by, created_at)
-
-    return jsonify({
-        'message': f'Endpoint created at {endpoint_path}',
-        'endpoint_path': endpoint_path,
-        'method': 'GET'
-    })
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        logging.error('Dynamic endpoint Generation API Error: {}'.format(e))
+        return jsonify({"error": "Internal server error"}), 500
 
 
 # Catch-all route for dynamic endpoints
 @app.route('/dynamic/<path:endpoint_name>', methods=['GET'])
 def handle_dynamic_endpoint(endpoint_name):
+    try:
+        if not endpoint_exists(endpoint_name):
+            return jsonify({'error': f'Endpoint /{endpoint_name} not found'}), 404
+        
+        filters = request.args.to_dict()
 
-    if not endpoint_exists(endpoint_name):
-        return jsonify({'error': f'Endpoint /{endpoint_name} not found'}), 404
+        filter_values = list(filters.values()) 
+        result = get_query_by_endpoint_name(endpoint_name)
+        query_template = result["query"]
+        endpoint_id = result["id"]
+
+        if not query_template:
+            return jsonify({'error': 'No query found for this endpoint'}), 404
+        
+        final_query = sql.SQL(query_template)
+
+        conn = psycopg2.connect(**DB_CONFIG)
+        cursor = conn.cursor()
+        cursor.execute(final_query, filter_values)
+        columns = [desc[0] for desc in cursor.description]
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        log_endpoint_usage(endpoint_id, accessed_at=get_current_datetime())
+        
+        return jsonify([dict(zip(columns, rows))]), 200
     
-    filters = request.args.to_dict()
-
-    filter_values = list(filters.values()) 
-    result = get_query_by_endpoint_name(endpoint_name)
-    query_template = result["query"]
-    endpoint_id = result["id"]
-
-    if not query_template:
-        return jsonify({'error': 'No query found for this endpoint'}), 404
-     
-    final_query = sql.SQL(query_template)
-
-    conn = psycopg2.connect(**DB_CONFIG)
-    cursor = conn.cursor()
-    cursor.execute(final_query, filter_values)
-    columns = [desc[0] for desc in cursor.description]
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
-
-    log_endpoint_usage(endpoint_id, accessed_at=get_current_datetime())
-    
-    return jsonify([dict(zip(columns, rows))]), 200
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        logging.error('Dynamic API Error: {}'.format(e))
+        return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
